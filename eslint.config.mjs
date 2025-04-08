42214874c9eb341c28a2a4730a9386a82c426675
BinaySaahu <100080926+BinaySaahu@users.noreply.github.com>
2025-04-08 18:43:19 +0530

    Eslint issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,26 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+// export default eslintConfig;
+
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const config = compat.config({
+  extends: ["next"],
+});
 
-export default eslintConfig;
+export default config;
